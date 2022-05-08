3f1452f55a40856ad39a81e7705366ed033b0512
Sourav Layek <souravlayek11@gmail.com>
2022-05-08 18:43:25 +0530

    added top seller scrolling

--- src/Components/PageComponents/HomePage/MainBody/fragments/TopSellers.tsx
+++ src/Components/PageComponents/HomePage/MainBody/fragments/TopSellers.tsx
@@ -1,30 +1,59 @@
-import { ProfileWithDetails,
-  ContentHeading } from "Components";
+import { ProfileWithDetails, ContentHeading } from "Components";
 import { NextArrowIcon, PreviousArrowIcon } from "Icons";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { TOP_SELLERS } from "utils";
 import styles from "./styles/topSeller.module.scss";
 
 type Props = {};
 
+
 const TopSellers = (props: Props) => {
+  const profileList = useRef<HTMLDivElement>(null);
+  const [isLeftPresent, setIsLeftPresent] = useState(false)
+  const [isRightPresent, setIsRightPresent] = useState(false)
+  const scrollLittle = (element: HTMLDivElement | null, isLeft?: boolean) => {
+    if (element) {
+      const currentScrollPosition = element.scrollLeft;
+      element.scroll({
+        top: 0,
+        left: currentScrollPosition + 100 * (isLeft ? -1 : 1),
+        behavior: "smooth",
+      });
+      setIsLeftPresent(element.scrollLeft  !== 0)
+      setIsRightPresent(element.scrollLeft  !== element.scrollWidth - element.clientWidth)
+    }
+  };
   return (
     <div className={styles.topSellers}>
       <ContentHeading title="Top Sellers" />
       <div className={styles.profiles}>
-        <div className={`${styles.scrollButton} ${styles.prevButton}`}>
-          <PreviousArrowIcon />
+        {isLeftPresent && (
+          <div
+            onClick={() => {
+              scrollLittle(profileList.current, true);
+            }}
+            className={`${styles.scrollButton} ${styles.prevButton}`}
+          >
+            <PreviousArrowIcon />
+          </div>
+        )}
+        <div ref={profileList} className={styles.profileList}>
+          {TOP_SELLERS.map((item) => (
+            <ProfileWithDetails
+              key={item.name}
+              hasNotification={false}
+              {...item}
+            />
+          ))}
         </div>
-        <div className={styles.profileList}>
-          {TOP_SELLERS.map((item,index) => <ProfileWithDetails
-          key={item.name}
-            hasNotification={false}
-            {...item}
-          />)}
-        </div>
-        <div className={`${styles.scrollButton} ${styles.nextButton}`}>
+       {isRightPresent && <div
+          onClick={() => {
+            scrollLittle(profileList.current);
+          }}
+          className={`${styles.scrollButton} ${styles.nextButton}`}
+        >
           <NextArrowIcon />
-        </div>
+        </div>}
       </div>
     </div>
   );
